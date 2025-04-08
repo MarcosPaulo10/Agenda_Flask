b552464c248683347941d50310704ce068ec99ec
Marcos Paulo Nogueira Mendes <mpnm10@hotmail.com>
2025-04-08 12:23:44 -0300

    Edit activities and others.

--- schedule/blueprints/activities/routes.py
+++ schedule/blueprints/activities/routes.py
@@ -58,6 +58,43 @@ def delete_activity(id):
     db.session.commit()
     
     return redirect(url_for('home.index'))
+
+@activity.route('/edit_activity/<int:id>', methods=['POST', 'GET'])
+def edit_activity(id):
+    activity = Activity.query.get(id)
+    categories = Category.query.all()
+    
+    if request.method == 'GET':
+        return render_template('activities/edit_activity.html', activity=activity, categories=categories)
+    
+    if request.method == 'POST':
+        activity.name = request.form.get('name')
+        activity.description = request.form.get('description')
+        
+        date = request.form.get('date')
+        formated_date = datetime.strptime(date, "%Y-%m-%d").date()
+        activity.date = formated_date
+        
+        start_time = request.form.get('start_time')
+        formated_start_time = datetime.strptime(start_time, "%H:%M").time() if start_time else None
+        activity.start_time = formated_start_time
+        
+        end_time = request.form.get('end_time')
+        formated_end_time = datetime.strptime(end_time, "%H:%M").time() if end_time else None
+        activity.end_time = formated_end_time
+        
+        activity.done = request.form.get("done") == "on"
+        activity.category = request.form.get('category_id')
+        
+        db.session.commit()
+        
+        return redirect(url_for('activity.activity_info', id=id))
+    
+@activity.route('/activity/<int:id>')
+def activity_info(id):
+    activity = Activity.query.get(id)
+    categories = Category.query.all()
+    return render_template('activities/activity_info.html', activity=activity, categories=categories)
     
 @activity.route('/category', methods=['POST', 'GET'])
 def index_category():
