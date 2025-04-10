1e77ca8c5efba7f73ca0a018fd4ca406f8921430
Marcos Paulo Nogueira Mendes <mpnm10@hotmail.com>
2025-04-10 19:14:55 -0300

    ----

--- schedule/blueprints/home/routes.py
+++ schedule/blueprints/home/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from schedule.app import db
 from schedule.blueprints.activities.models import Activity
 from datetime import datetime
@@ -7,5 +7,11 @@ home = Blueprint('home', __name__, template_folder='templates')
 
 @home.route('/')
 def index():
-    today_activities = db.session.query(Activity).filter(Activity.date == datetime.today().date()).all()
-    return render_template('home/index.html', activities = today_activities)
+    date_str = request.args.get('date')
+    if date_str:
+        selected_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+    else:
+        selected_date = datetime.today().date()
+
+    today_activities = db.session.query(Activity).filter(Activity.date == selected_date).all()
+    return render_template('home/index.html', activities=today_activities, selected_date=selected_date)
